2adf7e4724eef2b73a1c4a3cc87ef4cdf6e9d3c9
vedantparikh <vedantparikh63@gmail.com>
2023-01-22 14:44:33 +0100

    feature(0001): adds streamlit table

--- portfolio/python/streamlit/main.py
+++ portfolio/python/streamlit/main.py
@@ -1,6 +1,5 @@
 import os
 
-import requests
 import streamlit as st
 from alpha_vantage.timeseries import TimeSeries  # https://pypi.org/project/alpha-vantage/
 
@@ -9,12 +8,8 @@ st.title('Portfolio App')
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 search_url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey={API_KEY}'
-r = requests.get(search_url)
-data = r.json()
 
-print(data)
-
-ts = TimeSeries(key=API_KEY, output_format='pandas')
+ts = TimeSeries(key='T59Q1RPYX7I7MONU', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full')
 
-print(data)
+st.table(data)
