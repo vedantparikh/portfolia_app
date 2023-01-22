686ac645fae2960ccfb7f6f77db3a4a52d6e33fe
vedantparikh <vedantparikh63@gmail.com>
2023-01-22 20:33:12 +0100

    feature(0002): adds plotly plot which hides saturday sunday from the plot
    feature(0002): adds plotly plot and uses yahoo finance packages
    
    feature(0002): adds new api package

--- portfolio/python/streamlit/main.py
+++ portfolio/python/streamlit/main.py
@@ -1,28 +1,46 @@
-import os
-import requests
-from pprint import pprint
-
-from alpha_vantage.timeseries import TimeSeries  # https://pypi.org/project/alpha-vantage/
 import streamlit as st
+import yahooquery as yq
+import yfinance as yf
+import plotly.graph_objects as go
 
-API_KEY = os.getenv('API_KEY')
 st.title('Portfolio App')
 
-API_KEY = 'T60Q1RPYX7I7MONU'
+def get_symbols(query):
+    try:
+        data = yq.search(query)
+    except ValueError:
+        print(query)
+    else:
+        quotes = data['quotes']
+        if len(quotes) == 0:
+            return 'No Symbol Found'
+
+        symbol = []
+        for quote in quotes:
+            symbol.append((quote['symbol'], quote['longname'], quote['quoteType'], quote['exchange']))
+        return symbol
 
 keywords = st.text_input('Search by Script name ....')
 if keywords:
-    search_url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={keywords}&apikey={API_KEY}'
-    r = requests.get(search_url)
-    data = r.json()
-    print(data)
-    st.json(data)
-    matches = data.get('bestMatches')
+    matches = get_symbols(keywords)
+
     if matches:
-        symbols = tuple(match['1. symbol'] for match in matches)
-        symbol= st.selectbox('Select the relevant symbol', symbols)
+        symbols = tuple(match[0] for match in matches)
+        symbol = st.selectbox('Select the relevant symbol', symbols)
         st.write('You selected:', symbol)
-        u = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=SIEMENS&interval=5min&apikey={API_KEY}'
-        r = requests.get(u)
-        data = r.json()
-        st.json(data)
+        msft = yf.Ticker(symbol)
+        hist = msft.history(period="7d", interval='1m')
+        hist['date'] = hist.index
+        st.dataframe(hist)
+
+        # https://plotly.com/python/time-series/
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=hist.index, y=hist.Close))
+
+        fig.update_xaxes(
+            rangebreaks=[
+                dict(bounds=[15.30, 9.15], pattern="hour"),
+                dict(bounds=["sat", "mon"]),
+            ]
+        )
+        st.plotly_chart(fig, use_container_width=True)
