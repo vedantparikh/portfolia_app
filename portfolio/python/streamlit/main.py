2d5d53bc4304da5865d5c3ba58432fac969e1f90
vedantparikh <vedantparikh63@gmail.com>
2023-01-22 17:15:44 +0100

    feature(0001): adds search mechanism

--- portfolio/python/streamlit/main.py
+++ portfolio/python/streamlit/main.py
@@ -1,19 +1,40 @@
 import os
-
+import requests
+from pprint import pprint
 from alpha_vantage.timeseries import TimeSeries  # https://pypi.org/project/alpha-vantage/
-
+import yfinance as yf
 import streamlit as st
 
 API_KEY = os.getenv('API_KEY')
 st.title('Portfolio App')
 
+API_KEY = 'T60Q1RPYX7I7MONU'
+
 keywords = st.text_input('Search by Script name ....')
-ts = TimeSeries.get_symbol_search(keywords)
-st.selectbox(ts)
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-search_url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey={API_KEY}'
+if keywords:
+    search_url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={keywords}&apikey={API_KEY}'
+    r = requests.get(search_url)
+    data = r.json()
+    print(data)
+    matches = data.get('bestMatches')
+    if matches:
+        symbols = tuple(match['1. symbol'] for match in matches)
+        symbol= st.selectbox('Select the relevant symbol', symbols)
+        st.write('You selected:', symbol)
+        print(symbol)
+        u = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=1min&apikey={API_KEY}&outputsize=full'
+        r = requests.get(u)
+        data = r.json()
+        print(data)
+        st.json(data)
+        # # # ts = TimeSeries(key='T59Q1RPYX7I7MONU',output_format='pandas', indexing_type='date')
+        # data, meta_data = ts.get_intraday(symbol=symbol,interval='1min', outputsize='full')
+        # pprint(data.head(2))
+    # st.json(data)
 
-ts = TimeSeries(key='T59Q1RPYX7I7MONU', output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full')
 
-st.table(data)
+
+#ts = TimeSeries(key='T59Q1RPYX7I7MONU',output_format='pandas', indexing_type='date')
+#data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+#pprint(data.head(2))
+#st.text_area(data)
