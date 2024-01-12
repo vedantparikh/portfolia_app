51bdb8dbd3eeeb6c02827b90490c481ac34c57b8
vedant <bubbly@Vedants-Air.fritz.box>
2024-01-12 19:50:34 +0100

    feat(PA-7): attempt db initialzation 1

--- portfolio/python/api/authentication/database.py
+++ portfolio/python/api/authentication/database.py
@@ -1,9 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost:5432/portfolio"
+POSTGRES_USER: str = os.environ.get("POSTGRES_USER")  
+POSTGRES_PASSWORD: str = os.environ.get("POSTGRES_PASSWORD")  
+POSTGRES_HOST: str = os.environ.get("POSTGRES_HOST")
+POSTGRES_DB: str = os.environ.get("POSTGRES_DB")  
+DEBUG: bool = True if os.environ.get("DEBUG") == "True" else False
+
+if DEBUG:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+else:
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
